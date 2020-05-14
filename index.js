f841a413e1f594a35bd714f63d848ea07c2b126b
xdiuee <stronkina123@gmail.com>
2020-05-14 21:13:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,28 @@ bot.on('ready', () => {
     console.log('This bot is online!')
 })
 
-var version = '1.0.0';
+var version = '1.0.1';
+
+bot.on("guildMemberAdd", function(message){
+
+    let guild = message.guild;
+    let member = message;
+    let memberCount = client.user.size;
+
+    let embedJoin = new Discord.MessageEmbed()
+        .setColor('#09ff00')
+        .setTitle(`Cloud Bot`)
+        .setDescription(`Witaj ${member.user}! Na discordzie Cloud Bot`)
+        .setThumbnail(member.user.avatarURL);
+    
+    member.guild.channel.find(`name`, `welcome`).send({ embed: embedJoin });
+    
+})
+
+bot.on("guildMemberAdd", member => {
+    var memberRole = member.guild.roles.find(`name`, `Member`);
+    member.addRole(memberRole);
+})
 
 bot.on('message', message =>{
     
@@ -17,23 +38,28 @@ bot.on('message', message =>{
 
     switch (args[0]){
         case 'help':
-            
-            let icon = bot.user.displayAvatarURL();
+
             let embedHelp = new Discord.MessageEmbed()
 
-            embedHelp.setColor('#09ff00');
-            embedHelp.setTitle('Bot Information');
-            embedHelp.setThumbnail(icon);
-            embedHelp.addField('Name', bot.user.username);
-            embedHelp.addField('Stworzony przez', 'iuee');
+                .setColor('#09ff00')
+                .setTitle('Cloud Bot Help')
+                .addField('Sprawdź wersje bota.', '!version');
 
             message.channel.send(embedHelp);
-                
 
+            break;
+        case 'info':
+            
+            let icon = bot.user.displayAvatarURL();
+            let embedInfo = new Discord.MessageEmbed()
 
+            embedInfo.setColor('#09ff00');
+            embedInfo.setTitle('Bot Information');
+            embedInfo.setThumbnail(icon);
+            embedInfo.addField('Name', bot.user.username);
+            embedInfo.addField('Stworzony przez', 'iuee');
 
-
-
+            message.channel.send(embedInfo);
             break;
         case 'version':
             message.channel.send(`Version: ${version}`)
