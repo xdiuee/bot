9d7100a71d2c938e253297112eb7caf518740540
xdiuee <stronkina123@gmail.com>
2020-05-15 13:09:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ const token = process.env.token;
 
 const PREFIX = '!';
 
-const cheerio = require('cheerio');
-
-const request = require('request');
-
 bot.on('ready', () => {
     console.log(`${bot.user.username} zostal aktywowany!`)
     bot.user.setActivity('Cloud Bot', { type: `WATCHING` });
@@ -16,38 +12,7 @@ bot.on('ready', () => {
 
 var version = '1.2.0';
 
-function image(message){
-
-    var options = {
-        url: "https://results.dogpile.com/serp?qc=images&q=" + "cursed image",
-        method: "GET",
-        headers: {
-            "Accept": "text/html",
-            "User-Agent": "Chrome"
-        }
-    }
-
 }
-
-request(options, function(error, response, responseBody){
-    if(error){
-        return;
-    }
-
-    $ = cheerio.load(responseBody);
-
-    var links = $(".image a.link");
-
-    var urls = new Array(links.length).fill(0).map((v, i)=> links.eq(i).attr("href"));
-
-    console.log(urls);
-
-    if(!urls.length){
-        return;
-    }
-
-    message.channel.send(urls[0]);
-})
 
 bot.on('message', message =>{
     
